1cffaa84c547ca8debbb63a2c4914cb9a2e067ca
uosis <uosisl@gmail.com>
2020-08-22 11:31:41 -0600

    use slider and progress bar on the page

--- src/main/scala/webcomponents/WebComponentsPage.scala
+++ src/main/scala/webcomponents/WebComponentsPage.scala
@@ -2,13 +2,14 @@ package webcomponents
 
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
-import webcomponents.material.Button
+import webcomponents.material._
 
 object WebComponentsPage {
 
   def apply(): Div = {
     val actionVar = Var("Do the thing")
     val iconVar = Var("<>")
+    val progressVar = Var(0d)
 
     div(
       h1("Web Components"),
@@ -44,6 +45,22 @@ object WebComponentsPage {
           _.onMouseOver --> (_ => println("MouseOver")), // "custom" event
           _.slots.icon(span(child.text <-- iconVar.signal)),
           //_ => onMountCallback(ctx => ctx.thisNode.ref.doThing()) // doThing is not implemented, just for reference
+        )
+      ),
+      p(
+        div(
+          LinearProgressBar(
+            _.progress <-- progressVar.signal,
+          )
+        ),
+        div(
+          Slider(
+            _.pin := true,
+            _.min := 0,
+            _.max := 20,
+            // _ => onMountCallback(ctx => ctx.thisNode.ref.layout()), // failing with TypeError: Cannot read property 'layout' of undefined
+            _.onInput --> (ev => progressVar.set(ev.target.asInstanceOf[Slider.Ref].value / 20)), // is it possible to avoid the cast here?
+          )
         )
       )
     )
