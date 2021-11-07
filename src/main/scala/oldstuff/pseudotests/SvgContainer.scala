aa431a7185b79454f63bbd366cd22a6ab19cf69a
Nikita Gazarov <nikita@raquo.com>
2021-11-07 00:52:17 -0700

    Misc: Add foreignObject svg example

--- src/main/scala/oldstuff/pseudotests/SvgContainer.scala
+++ src/main/scala/oldstuff/pseudotests/SvgContainer.scala
@@ -7,17 +7,41 @@ object SvgContainer {
 
   def apply(): HtmlElement = {
     div(
-      "SVG!!!",
-      s.svg(
-        // Note: you generally don't need to specify xmlns attribute when building SVGs with scripts (as opposed to downloading)
-        s.xmlns := "http://www.w3.org/2000/svg", // @TODO SvgNamespaces.svg constant when it's available
-        s.height := "200",
-        s.width := "400",
-        s.circle(
-          s.cx := "200",
-          s.cy := "15",
-          s.r := "30",
-          s.fill := "red"
+      h2("Offset circle"),
+      simpleSvg(),
+      h2("Foreign object (HTML)"),
+      foreignObjectSvg()
+    )
+  }
+
+  def simpleSvg(): SvgElement = {
+    s.svg(
+      // Note: you generally don't need to specify xmlns attribute when building SVGs with scripts (as opposed to downloading)
+      s.xmlns := "http://www.w3.org/2000/svg", // @TODO SvgNamespaces.svg constant when it's available
+      s.height := "200",
+      s.width := "400",
+      s.circle(
+        s.cx := "200",
+        s.cy := "15",
+        s.r := "30",
+        s.fill := "red"
+      )
+    )
+  }
+
+  def foreignObjectSvg(): SvgElement = {
+    s.svg(
+      s.height := "200",
+      s.width := "400",
+      s.polygon(s.points("5,5 195,10 185,175 10,195"), s.fill("yellow")),
+      s.foreignObject(
+        s.x("20"),
+        s.y("20"),
+        s.width("160"),
+        s.height("160"),
+        div(
+          //xmlns("http://www.w3.org/1999/xhtml"),
+          "Hello!"
         )
       )
     )
